65bacb5dda7279efcffa7565b1948be6903810a4
Rafael Barbosa <rafaelrodrigues0404@gmail.com>
2026-04-24 02:09:07 -0300

    atualizacoes antes do pull

--- main.py
+++
@@ -1,6 +0,0 @@
-from assistente_leilao.interface.gui import iniciar_gui
-from assistente_leilao.logging_config import configurar_logging
-
-if __name__ == "__main__":
-    configurar_logging()
-    iniciar_gui()
